4690a125db49a69de6c929d021c59e681b2d7074
Sudipta Chowdhury <sudipta.chowdhury@welldev.io>
2024-06-06 12:25:05 +0600

    Initial Commit

---
+++ IOS_Animation_AppUITests/IOS_Animation_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IOS_Animation_AppUITestsLaunchTests.swift
+//  IOS_Animation_AppUITests
+//
+//  Created by Sudipta Chowdhury on 6/6/24.
+//
+
+import XCTest
+
+final class IOS_Animation_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
