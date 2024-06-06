ae3a0f46d644e41bcbc38eb81ef07cc3e24fa82e
Sudipta Chowdhury <sudipta.chowdhury@welldev.io>
2024-06-06 12:37:34 +0600

    image animation added

--- IOS_Animation_App/ViewController.swift
+++ IOS_Animation_App/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var birdImages: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createImageArray()
+    }
+    
+    private func createImageArray() {
+        for count in 1...5 {
+            let imageName = "bird\(count).png"
+            let image = UIImage(named: imageName)!
+            birdImages.append(image)
+        }
     }
 
-
+    @IBAction func startButtonTapped(_ sender: Any) {
+        imageView.animationImages = birdImages
+        imageView.animationDuration = 1.0
+        imageView.animationRepeatCount = 4
+        imageView.startAnimating()
+    }
 }
 
